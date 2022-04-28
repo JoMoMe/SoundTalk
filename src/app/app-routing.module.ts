a9af6b866ead76d058fea2ef83e6eff8866b98dc
Joan Molina <jomome@fp.insjoaquimmir.cat>
2022-04-28 20:02:28 +0200

    Register + login update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingpageComponent } from './components/landingpage/landingpage.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   {path : '', component : LandingpageComponent},
   {path : 'landingpage', component : LandingpageComponent},
   {path : 'login', component : LoginComponent},
+  {path : 'register', component : RegisterComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingpageComponent } from './components/landingpage/landingpage.component';
 import { LoginComponent } from './components/login/login.component';
-//import { UserComponent } from './components/user/user.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingpageComponent,
     LoginComponent,
-    //UserComponent
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
