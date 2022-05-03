dcec1887fa3926f0029b4f411de0fc242c0b6c60
Joan Molina <jomome@fp.insjoaquimmir.cat>
2022-05-03 17:44:17 +0200

    Landing page v4

--- src/app/components/landingpage/landingpage.component.ts
+++ src/app/components/landingpage/landingpage.component.ts
@@ -1,5 +1,5 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-
+import { Component, ViewEncapsulation, ViewChild } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-landingpage',
   templateUrl: './landingpage.component.html',
@@ -8,6 +8,18 @@ import { Component, ViewEncapsulation } from '@angular/core';
 })
 export class LandingpageComponent {
 
-  constructor() { }
+  images = [
+    {src: "/assets/img/Foto_Landing_4.png"},
+    {src: "https://picsum.photos/id/1011/900/500"},
+    {src: "https://picsum.photos/id/984/900/500"}
+  ];
+
+  constructor(config: NgbCarouselConfig) {
+    // customize default values of carousels used by this component tree
+    config.interval = 5000;
+    config.wrap = false;
+    config.keyboard = false;
+    config.pauseOnHover = false;
+  }
 
 }
