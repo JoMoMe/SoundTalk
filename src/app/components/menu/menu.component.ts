d2fa6ffb759397e9c54212a1db188c32a2cf45a7
Oliver Bocanegra Pérez <olbope@fp.insjoaquimmir.cat>
2022-05-06 21:07:05 +0200

    upload images 50%

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,8 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 /* import {SidebarModule } from 'cdbangular'; */
-import { Router } from '@angular/router';
+import { Event, Router } from '@angular/router';
 import { RegisterAndLoginService } from '../../services/users/users.service'
+import { PostsService } from '../../services/posts/posts.service'
 import { CookieService } from 'ngx-cookie-service';
+import { NgForm } from '@angular/forms';
+import { ObjectId } from 'mongodb';
+import { NgbNavChangeEvent } from '@ng-bootstrap/ng-bootstrap';
+import { ReadVarExpr } from '@angular/compiler';
+import { DomSanitizer } from '@angular/platform-browser';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-menu',
@@ -11,7 +18,7 @@ import { CookieService } from 'ngx-cookie-service';
 })
 export class MenuComponent implements OnInit {
 
-  constructor(public registerandloginService: RegisterAndLoginService, public router: Router, public cookie: CookieService) { }
+  constructor(private http: HttpClient, public registerandloginService: RegisterAndLoginService, public postsService: PostsService, public router: Router, public cookie: CookieService, public sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
     var cookiefound = this.cookie.get('cookieSoundTalkSession')
@@ -31,8 +38,65 @@ export class MenuComponent implements OnInit {
     }      
   }
 
-  createPost(form: NgForm){
+  public files: any = []
+  public previsualize: any
 
+  captureFile(event: any): any{
+    var file = event.target.files[0]
+    this.converttoBase64(file).then((image: any) => {
+      this.previsualize = image.base;
+      console.log(image)
+    })
+    this.files.push(file)
   }
 
+  converttoBase64 = async ($event: any) => new Promise((resolve, reject) => {
+    try{
+      const unsafeImg = window.URL.createObjectURL($event)
+      const image = this.sanitizer.bypassSecurityTrustUrl(unsafeImg)
+      const reader = new FileReader()
+      reader.readAsDataURL($event)
+      reader.onload = () => {
+        resolve({
+          base: reader.result
+        })
+      }
+      reader.onerror = error => {
+        resolve({
+          base: null
+        })
+      }
+    }
+    catch (e){
+      reject(e)
+    }
+  })
+
+  uploadFile(): any{
+/*     try{
+      const formulary = new FormData()
+      this.files.forEach(element => {
+        console.log(element)
+        formulary.append('photo', element) 
+      })
+
+      this.http.post('http://localhost:4005/menu', formulary).subscribe(
+        res => {
+          console.log(res)
+        },
+        err => console.error(err)
+      )
+    }
+    catch (err){
+      console.log(err)
+    } */
+  }
+
+  createPost(form: NgForm){
+    this.postsService.createPost(form.value, this.cookie.get('cookieSoundTalkSession')).subscribe(
+      res => {console.log(res)
+      },
+      err => console.error(err)
+    )
+  }
 }
--- src/app/services/posts/posts.service.ts
+++ src/app/services/posts/posts.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { posts as Posts } from 'src/app/models/posts';
+import { Form, NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
+  constructor(private http: HttpClient, public router: Router) {}
 
-  constructor() { }
+  url_menu = 'http://localhost:4005/menu'
+
+  createdPost: Posts =  {
+    title: '',
+    content: '',
+    photofile: '',
+    audiofile: '',
+    userid: JSON.parse(JSON.stringify('a')),
+    likes: 0,
+    comments: 0,
+    commentsid: JSON.parse(JSON.stringify('a')),
+  };
+  
+  createPost(posts: Posts, userid: string){
+    const idstring = JSON.stringify(userid)
+    const id = JSON.parse(idstring)
+    posts.userid=id
+    console.log(posts)
+    return this.http.post(this.url_menu, posts)
+  }
 }
