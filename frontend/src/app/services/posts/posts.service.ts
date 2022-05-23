669a38e04b90fe3198375ecaae2335ccddd45f5c
Joan Molina <jomome@fp.insjoaquimmir.cat>
2022-05-23 19:26:28 +0200

    backend

--- frontend/src/app/services/posts/posts.service.ts
+++ frontend/src/app/services/posts/posts.service.ts
@@ -6,6 +6,7 @@ import { likes as Likes } from 'src/app/models/likes';
 import { Form, NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ObjectId } from 'mongodb';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
@@ -13,10 +14,10 @@ import { ObjectId } from 'mongodb';
 export class PostsService {
   constructor(private http: HttpClient, public router: Router) {}
 
-  url_menu = '/menu'
-  url_photo = '/menu/photos'
-  url_audio = '/menu/audio'
-  url_myposts = '/menu/profile/user/'
+  url_menu = environment.api + '/menu'
+  url_photo = environment.api + '/menu/photos'
+  url_audio = environment.api + '/menu/audio'
+  url_myposts = environment.api + '/menu/profile/user/'
 
   createdPost: Posts =  {
     title: '',
@@ -48,11 +49,11 @@ export class PostsService {
   }
 
   getPhoto(id: string){
-    return this.http.get('/menu/photos/'+id)
+    return this.http.get(environment.api + '/menu/photos/'+id)
   }
 
   getAudio(id: string){
-    return this.http.get('/menu/audio/'+id, {responseType: 'blob'})
+    return this.http.get(environment.api + '/menu/audio/'+id, {responseType: 'blob'})
   }
 
   createPost(posts: Posts, userid: string, audioid: string, photoid: string, posttype: string){
@@ -83,16 +84,16 @@ export class PostsService {
     const idusercomment = JSON.parse(idstring)
 
     comments.userid=idusercomment
-    return this.http.post('/menu/posts/'+postid+'/comment', comments)
+    return this.http.post(environment.api + '/menu/posts/'+postid+'/comment', comments)
   }
 
   putLikeinPost(userid: string, postid: string){
     const iduser = {'id': userid};
-    return this.http.post('/menu/posts/'+postid+'/like', iduser)
+    return this.http.post(environment.api + '/menu/posts/'+postid+'/like', iduser)
   }
 
   deleteComment(commentid: string, postid: string){
-    return this.http.delete('/menu/posts/'+postid+'/comment/'+commentid)
+    return this.http.delete(environment.api + '/menu/posts/'+postid+'/comment/'+commentid)
   }
 
   deleteAPost(userid: string, postid: string){
@@ -104,6 +105,6 @@ export class PostsService {
     const idusercomment = JSON.parse(idstring)
 
     comments.userid=idusercomment
-    return this.http.post('/menu/posts/'+postid+'/comment', comments)
+    return this.http.post(environment.api + '/menu/posts/'+postid+'/comment', comments)
   }
 }
